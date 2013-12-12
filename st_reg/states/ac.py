ee60683ff687ff0734751e748af086ad06fb86dd
matheuscas <matheus.mcas@gmail.com>
2013-12-12 13:42:54 -0300

    added algorithm

--- st_reg/states/ac.py
+++ st_reg/states/ac.py
@@ -1,3 +1,39 @@
+
 def check(st_reg_number):
-    """Docstring"""
-    pass
+    """Checks the number valiaty for the Acre state"""
+    #st_reg_number = str(st_reg_number)
+    weights = [4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2]
+    digits = st_reg_number[:len(st_reg_number) - 2]
+    check_digits = st_reg_number[-2:]
+    DIVISOR = 11
+
+    if len(st_reg_number) > 13:
+        return False
+
+    sum = 0
+    for i in range(len(digits)):
+        sum = sum + int(digits[i]) * weights[i]
+
+    rest_division = sum % DIVISOR
+    first_digit = DIVISOR - rest_division
+
+    if first_digit == 10 or first_digit == 11:
+        first_digit = 0
+
+    if str(first_digit) != check_digits[0]:
+        return False
+
+    digits = digits + str(first_digit)
+    weights = [5] + weights
+
+    sum = 0
+    for i in range(len(digits)):
+        sum = sum + int(digits[i]) * weights[i]
+
+    rest_division = sum % DIVISOR
+    second_digit = DIVISOR - rest_division
+
+    if second_digit == 10 or second_digit == 11:
+        second_digit = 0
+        
+    return str(first_digit) + str(second_digit) == check_digits    
