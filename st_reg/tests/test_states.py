4d02330ee373551babbff28e0f9a2ce4d8f746ec
matheuscas <matheus.mcas@gmail.com>
2013-12-12 13:43:24 -0300

    added testes for Acre state

--- st_reg/tests/test_states.py
+++ st_reg/tests/test_states.py
@@ -5,8 +5,29 @@ import os
 import sys
 sys.path.append(os.path.abspath(os.path.dirname(__file__) + '/' + '../..'))
 
-import st_reg.states.ac as ac 
+import st_reg.states.ac as ac
 
+#ACRE
 def test_ac_validation_right_size_invalid_number():
-    """Docstring"""
-    pass
+    """Test if a invalid number is really invalid"""
+
+    invalid_number = '0172030964577'
+    assert ac.check(invalid_number) == False
+
+def test_ac_validation_right_size_valid_number():
+    """Test if a valid number is really valid"""
+
+    valid_number = '0172030964575'
+    assert ac.check(valid_number)
+
+def test_ac_validation_small_size_number():
+    """Test if a invalid number, with wrong size, is really invalid"""
+
+    invalid_number = '0172030'
+    assert ac.check(invalid_number) == False
+
+def test_ac_validation_big_size_number():
+    """Test if a invalid number, with wrong size, is really invalid"""
+
+    invalid_number = '01720309645755'
+    assert ac.check(invalid_number) == False 
